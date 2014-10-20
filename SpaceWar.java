2e056445b5306d32ffeddacff3519edf5544ba26
afroK <christoferen7@live.com>
2014-10-20 13:04:55 -0600

    Updating hero images sprites

---
+++ SpaceWar.java
@@ -0,0 +1,14 @@
+/*
+		Author: Khriz Enríquez	@khrizenriquez
+
+		Image sprites: http://sm.ingenieriamultimedia.org/lib/exe/fetch.php?media=alumnos:grupo_13:naves.png
+*/
+
+import javax.swing.JFrame;
+
+public class SpaceWar {
+	public static void main(String[] args) {
+		/*		JFrame will be my container*/
+		
+	}
+}
